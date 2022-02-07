4d085437fbda16a55fbce894fcda080eaa433c9e
romulo telles <romulo telles@LAPTOP-KLP3KHK3>
2022-02-07 17:21:46 -0300

    Add project files.

---
+++ Exercício01.ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercício01.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor de A: ");
+            float valora = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de B: ");
+            float valorb = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de C: ");
+            float valorc = float.Parse(Console.ReadLine());
+
+            float somaab;
+            
+            somaab = valora + valorb;
+            
+            if (somaab < valorc)
+            {
+                Console.WriteLine("A soma de A + B é menor que o valor de C. ");
+
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
